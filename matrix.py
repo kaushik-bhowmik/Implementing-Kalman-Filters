893e2d69d2f4faeff770655f944988ae009144c5
Kaushik Bhowmik <kaushik006@e.ntu.edu.sg>
2020-07-16 10:28:03 +0800

    first commit

---
+++ matrix.py
@@ -0,0 +1,217 @@
+import math
+from math import sqrt
+import numbers
+
+def zeroes(height, width):
+        """
+        Creates a matrix of zeroes.
+        """
+        g = [[0.0 for _ in range(width)] for __ in range(height)]
+        return Matrix(g)
+
+def identity(n):
+        """
+        Creates a n x n identity matrix.
+        """
+        I = zeroes(n, n)
+        for i in range(n):
+            I.g[i][i] = 1.0
+        return I
+
+class Matrix(object):
+
+    # Constructor
+    def __init__(self, grid):
+        self.g = grid
+        self.h = len(grid)
+        self.w = len(grid[0])
+
+    #
+    # Primary matrix math methods
+    #############################
+ 
+    def determinant(self):
+        """
+        Calculates the determinant of a 1x1 or 2x2 matrix.
+        """
+        if not self.is_square():
+            raise(ValueError, "Cannot calculate determinant of non-square matrix.")
+        if self.h > 2:
+            raise(NotImplementedError, "Calculating determinant not implemented for matrices largerer than 2x2.")
+        
+        if self.h==1:
+            return self.g[0]
+        else:
+            return (self.g[0][0]*self.g[1][1]) -  (self.g[0][1]*self.g[1][0])
+            
+
+    def trace(self):
+        """
+        Calculates the trace of a matrix (sum of diagonal entries).
+        """
+        if not self.is_square():
+            raise(ValueError, "Cannot calculate the trace of a non-square matrix.")
+        n_k=self.h
+        som=0
+        for i in range(n_k):
+            som=som+self.g[i][i]
+        return som
+            
+            
+
+    def inverse(self):
+        """
+        Calculates the inverse of a 1x1 or 2x2 Matrix.
+        """
+        
+        if not self.is_square():
+            raise(ValueError, "Non-square Matrix does not have an inverse.")
+        if self.h > 2:
+            raise(NotImplementedError, "inversion not implemented for matrices larger than 2x2.")
+        
+
+        if self.w==1:
+            
+            return Matrix([[1/self.g[0][0]]])
+        else:
+            inverse_m = zeroes(self.h, self.w)
+            a= self.g[0][0]
+            b= self.g[0][1]
+            c= self.g[1][0]
+            d= self.g[1][1]
+            
+            inverse_m[0][0]=(1/self.determinant())*d
+            inverse_m[0][1]=-(1/self.determinant())*b
+            inverse_m[1][0]=-(1/self.determinant())*c
+            inverse_m[1][1]=(1/self.determinant())*a
+                
+            return inverse_m
+                
+                
+                
+                
+        
+
+    def T(self):
+        """
+        Returns a transposed copy of this Matrix.
+        """
+        m_transpose = []
+        for i in range(self.w):
+            r=[]
+            for j in range(self.h):
+                r.append(self[j][i])
+            m_transpose.append(r)
+        return Matrix(m_transpose)
+    
+    
+
+    def is_square(self):
+        return self.h == self.w
+
+    #
+    # Begin Operator Overloading
+    ############################
+    def __getitem__(self,idx):
+        """
+        Defines the behavior of using square brackets [] on instances
+        of this class.
+
+        Example:
+
+        > my_matrix = Matrix([ [1, 2], [3, 4] ])
+        > my_matrix[0]
+          [1, 2]
+
+        > my_matrix[0][0]
+          1
+        """
+        return self.g[idx]
+
+    def __repr__(self):
+        """
+        Defines the behavior of calling print on an instance of this class.
+        """
+        s = ""
+        for row in self.g:
+            s += " ".join(["{} ".format(x) for x in row])
+            s += "\n"
+        return s
+
+    def __add__(self,other):
+        """
+        Defines the behavior of the + operator
+        """
+        if self.h != other.h or self.w != other.w:
+            raise(ValueError, "Matrices can only be added if the dimensions are the same") 
+        #   
+        result_a=zeroes(self.h,self.w)
+        for i in range(self.h):
+            for j in range(self.w):
+                result_a[i][j]=self[i][j]+other[i][j]
+        return result_a
+
+    def __neg__(self):
+        """
+        Defines the behavior of - operator (NOT subtraction)
+
+        Example:
+
+        > my_matrix = Matrix([ [1, 2], [3, 4] ])
+        > negative  = -my_matrix
+        > print(negative)
+          -1.0  -2.0
+          -3.0  -4.0
+        """
+        result_n= zeroes(self.h,self.w) 
+        for i in range(self.h):
+            for j in range(self.w):
+                result_n[i][j]=-1*self.g[i][j]
+        return result_n
+        
+
+    def __sub__(self, other):
+        """
+        Defines the behavior of - operator (as subtraction)
+        """
+        #   
+        result_s=zeroes(self.h,self.w)
+        for i in range(self.h):
+            for j in range(self.w):
+                result_s[i][j]=self[i][j]-other[i][j]
+        return result_s
+
+    def __mul__(self, other):
+        """
+        Defines the behavior of * operator (matrix multiplication)
+        """
+        multiply = zeroes(self.h, other.w)
+        for i in range(multiply.h):
+            for j in range(multiply.w):
+                p_result = 0
+                for k in range(self.w):
+                    p_result += self[i][k]*other[k][j]
+                multiply[i][j] = p_result
+        return multiply
+
+    def __rmul__(self, other):
+        """
+        Called when the thing on the left of the * is not a matrix.
+
+        Example:
+
+        > identity = Matrix([ [1,0], [0,1] ])
+        > doubled  = 2 * identity
+        > print(doubled)
+          2.0  0.0
+          0.0  2.0
+        """
+        if isinstance(other, numbers.Number):
+            pass
+            
+            r_multiply = zeroes(self.h, self.w)
+            for i in range(self.h):
+                for j in range(self.w):
+                    r_multiply[i][j] = self.g[i][j] * other
+            return r_multiply
+            
